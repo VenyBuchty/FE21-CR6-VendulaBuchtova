8662be91bc3e85cc5e642ca5554879a53368e4d3
Vendula Buchtova <ven.buchtova@gmail.com>
2021-03-26 16:58:39 +0100

    travels done

---
+++ travelAgency/src/app/travel-offers.ts
@@ -0,0 +1,56 @@
+export const travelOffers = [
+  {
+    name: "Hotel Vara Cubo",
+    destination: "Cuba, Varadero",
+    description: "Stunning 7,000-acre hotel, resort, and residential community in La Romana on the southeastern coast of the Dominican Republic.Express yourself in an environment that surrounds you with every kind of pleasure.",
+    img: "assets/img/hotel1.jpg",
+    startDate: "04.04.2021",
+    duration: "14 days",
+    price: "1349€/person"
+  },
+  {
+    name: "Excellence El Carmen",
+    destination: "Dominican Republic, Punta Cana",
+    description: "At Excellence El Carmen you are secluded but not isolated. Our all-adult, all-suite world is situated on a beautiful Caribbean beach in a palm grove on the east coast of the Dominican Republic. ",
+    img: "assets/img/hotel6.jpg",
+    startDate: "01.05.2021",
+    duration: "16 days",
+    price: "1560€/person"
+  },
+  {
+    name: "Melia Cayo Coco",
+    destination: "Jardines del Ávila, Cuba",
+    description: "Located beachfront in Cayo Coco, habitat of a large colony of pink flamingos. Five-Star All Inclusive Resort with 250 rooms. Conceived only for adults aged 18 years or more.",
+    img: "assets/img/hotel3.jpg",
+    startDate: "30.03.2021",
+    duration: "14 days",
+    price: "1260€/person"
+  },
+  {
+    name: "Hotel LaCroix",
+    destination: "Honolulu, Hawai",
+    description: "Discover a variety of room accommodations tailored just for you. Our rooms are one of the most spacious in Waikiki, and include room categories including our luxurious Skyline Penthouses",
+    img: "assets/img/hotel7.jpg",
+    startDate: "13.05.2021",
+    duration: "15 days",
+    price: "1669€/person"
+  },
+  {
+    name: "Sun Siyam Iru Veli",
+    destination: "South Nilandhe Atoll, Maldives",
+    description: "Sleek and spacious accommodations make Sun Siyam Iru Veli the ultimate tropical retreat. With front row lagoon views and a freshwater pool for every five star suite, all you have to do is check in.",
+    img: "assets/img/hotel5.jpg",
+    startDate: "14.06.2021",
+    duration: "16 days",
+    price: "1490€/person"
+  },
+  {
+    name: "Fontainebleau",
+    destination: " Collins Ave, Miami Beach",
+    description: "A spectacular blend of Golden Era glamour and modern luxury, Fontainebleau today reinvents the original vision of legendary architect Morris Lapidus – a stage where everyone plays their unique part.",
+    img: "assets/img/hotel2.jpg",
+    startDate: "20.06.2021",
+    duration: "15 days",
+    price: "1230€/person"
+  }
+ ];
--- travelAgency/src/app/travels/travels.component.ts
+++ travelAgency/src/app/travels/travels.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { travelOffers } from '../travel-offers'
 
 @Component({
   selector: 'travels',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./travels.component.scss']
 })
 export class TravelsComponent implements OnInit {
-
+travelOffers = travelOffers;
   constructor() { }
 
   ngOnInit(): void {
